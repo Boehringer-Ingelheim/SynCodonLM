9a773d9e44d1ea6e94e872cbabf41161edb54a28
James Heuschkel <54549461+jheuschkel@users.noreply.github.com>
2026-08-28 12:38:01 -0400

    Refactor __all__ exports in __init__.py

--- SynCodonLM/__init__.py
+++ SynCodonLM/__init__.py
@@ -1,10 +1,12 @@
-from.get_embeddings import CodonEmbeddings
+from .get_embeddings import CodonEmbeddings
 from .optimize_sequence import CodonOptimizer
 from .utils import clean_split_sequence, synonymous_codons
 from .species_token_type import species_token_type
 
-__all__ = ["CodonEmbeddings"]
-__all__ = ["CodonOptimizer"]
-__all__ = ["clean_split_sequence"]
-__all__ = ["synonymous_codons"]
-__all__ = ["species_token_type"]
+__all__ = [
+    "CodonEmbeddings",
+    "CodonOptimizer",
+    "clean_split_sequence",
+    "synonymous_codons",
+    "species_token_type",
+]
